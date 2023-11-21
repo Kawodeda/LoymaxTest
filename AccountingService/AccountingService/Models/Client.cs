829d0311e301ae60376e1ae89f6b2205405beb56
Kawodeda <konserva023@gmail.com>
2023-11-21 19:48:05 +0700

    add data model

---
+++ AccountingService/AccountingService/Models/Client.cs
@@ -0,0 +1,24 @@
+﻿namespace AccountingService.Models
+{
+    public class Client
+    {
+        public Client(int id, string lastName, string firstName, string middleName, DateOnly birthDate)
+        {
+            Id = id;
+            LastName = lastName;
+            FirstName = firstName;
+            MiddleName = middleName;
+            BirthDate = birthDate;
+        }
+
+        public int Id { get; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; }
+
+        public string MiddleName { get; }
+
+        public DateOnly BirthDate { get; }
+    }
+}
---
+++ AccountingService/AccountingService/Models/Wallet.cs
@@ -0,0 +1,17 @@
+﻿namespace AccountingService.Models
+{
+    public class Wallet
+    {
+        public Wallet(int id, decimal amount)
+        {
+            Id = id;
+            Amount = amount;
+        }
+
+        public int Id { get; }
+
+        public decimal Amount { get; }
+
+        public Client? Client { get; }
+    }
+}
