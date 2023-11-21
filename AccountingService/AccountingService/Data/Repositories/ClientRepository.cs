12648be55b7dc2106b00cf30738cba266b08748c
Kawodeda <konserva023@gmail.com>
2023-11-21 22:04:53 +0700

    implement client repository

---
+++ AccountingService/AccountingService/Data/Repositories/ClientRepository.cs
@@ -0,0 +1,55 @@
+﻿using AccountingService.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace AccountingService.Data.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly AccountingDbContext _context;
+
+        public ClientRepository(AccountingDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Client> List()
+        {
+            return _context.Clients;
+        }
+
+        public async Task<Client?> Read(int id)
+        {
+            return await _context.Clients.FindAsync(id);
+        }
+
+        public async Task<Client?> ReadWithWallet(int id)
+        {
+            Client? client = await _context.Clients.FindAsync(id);
+
+            if (client != null)
+            {
+                await _context.Entry(client)
+                    .Reference(c => c.Wallet)
+                    .LoadAsync();
+            }
+
+            return client;
+        }
+
+        public async Task<Client> Create(Client client)
+        {
+            EntityEntry<Client> entry =  await _context.Clients.AddAsync(client);
+
+            await _context.SaveChangesAsync();
+
+            return entry.Entity;
+        }
+
+        public async Task Update(int id, Client client)
+        {
+            _context.Clients.Update(client);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ AccountingService/AccountingService/Data/Repositories/IClientRepository.cs
@@ -0,0 +1,17 @@
+﻿using AccountingService.Models;
+
+namespace AccountingService.Data.Repositories
+{
+    public interface IClientRepository
+    {
+        IEnumerable<Client> List();
+
+        Task<Client?> Read(int id);
+
+        Task<Client?> ReadWithWallet(int id);
+
+        Task<Client> Create(Client client);
+
+        Task Update(int id, Client client);
+    }
+}
